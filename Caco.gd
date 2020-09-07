1e559636fc5a14579f509777b071ca28f65ad980
Pedro SOUSA <bernardodesousa@gmail.com>
2020-09-07 15:56:52 -0300

    Implement fallzone

--- Caco.gd
+++ Caco.gd
@@ -3,11 +3,14 @@ extends KinematicBody2D
 var velocity = Vector2(0, 0)
 const GRAVITY = 30
 const SPEED = 180
-const JUMPFORCE = -900
+const JUMPFORCE = -1000
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
+		$Sprite.play("walk")
+	elif Input.is_action_pressed("left"):
+		velocity.x = -SPEED
 		$Sprite.play("walk")
 	else:
 		$Sprite.play("iddle")
@@ -23,3 +26,7 @@ func _physics_process(_delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	velocity.x = lerp(velocity.x, 0, 0.1)
+
+
+func _on_fallzone_body_entered(body):
+	get_tree().change_scene("res://level1.tscn")
