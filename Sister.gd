85c397c199b1ae638a2e1454a068ec4971635c9f
Pedro SOUSA <bernardodesousa@gmail.com>
2020-09-27 18:45:56 -0300

    Implement some rubberbanding

--- Sister.gd
+++ Sister.gd
@@ -1,4 +1,8 @@
 extends StaticBody2D
 
+onready var Player = get_tree().get_root().find_node("Caco", true, false)
+
 func _physics_process(_delta):
 	position += Vector2(2, 0)
+	if Player.position.x - position.x > 600:
+		position += Vector2(4, 0)
